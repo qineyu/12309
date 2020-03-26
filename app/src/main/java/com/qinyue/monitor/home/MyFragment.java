9666d6a1874daf06de8fa0e2e882d238b4711aaa
qinyue <1203825127@qq.com>
2020-03-26 10:14:58 +0800

    修改我的页面

--- app/src/main/java/com/qinyue/monitor/home/MyFragment.java
+++ app/src/main/java/com/qinyue/monitor/home/MyFragment.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.qinyue.monitor.R;
+import com.xuexiang.xui.widget.imageview.RadiusImageView;
+import com.xuexiang.xui.widget.textview.supertextview.SuperTextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
@@ -21,6 +26,12 @@ import butterknife.Unbinder;
 public class MyFragment extends Fragment {
     Unbinder unbinder;
     MainActivity mainActivity;
+    @BindView(R.id.riv_head_pic)
+    RadiusImageView headerImageView;
+    @BindView(R.id.text_login)
+    TextView loginText;
+    @BindView(R.id.view_logout)
+    SuperTextView logoutView;
     public MyFragment(){
 
     }
@@ -38,6 +49,43 @@ public class MyFragment extends Fragment {
     private void initOnCliclek() {
     }
 
+    @OnClick({R.id.view_login,R.id.view_my,R.id.view_msg,R.id.view_xf,R.id.view_yy,R.id.view_qzyjx,R.id.view_jczxx,R.id.view_logout})
+    public void onCkick(View view){
+        switch (view.getId()){
+            case R.id.view_login:{//登录
+
+            }
+            break;
+            case R.id.view_my:{//我的信息
+
+            }
+            break;
+            case R.id.view_msg:{//登录
+
+            }
+            break;
+            case R.id.view_xf:{//登录
+
+            }
+            break;
+            case R.id.view_yy:{//登录
+
+            }
+            break;
+            case R.id.view_jczxx:{//登录
+
+            }
+            break;
+            case R.id.view_logout:{//登录
+
+            }
+            break;
+            case R.id.view_qzyjx:{//退出登录
+
+            }
+            break;
+        }
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
